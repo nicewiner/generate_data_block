a19b8bcf36e9257dcfe2da1fd116e61c526e1181
xudi <xudi1989@ruc.edu.cn>
2017-02-17 20:03:19 +0800

    init

--- create_config.py
+++ create_config.py
@@ -2,7 +2,13 @@ import json
 
 base_file = r'E:\autoBackTest\block.config'
 
-text = {'start_date':20170101,'end_date':20170102,'indicators':["lastPrice","Volume"],'instruments':['if0001','if0002']}
+text1 = {'start_date':20160101,'end_date':20160102,'indicators':["lastPrice","Volume"],'instruments':['if0001','if0002']}
+text2 = {'start_date':20170101,'end_date':20170102,'indicators':["lastPrice","Volume"],'instruments':['if0001','if0002']}
 
-with open(base_file,'a+') as fout:
-    readed = json.dump(fout,text)
+with open(base_file,'w') as fout:
+    table = {'0':text1,'1':text2}
+    json.dump(table,fout)
+    
+with open(base_file,'r') as fin:
+    readed = json.load(fin)
+    print readed
